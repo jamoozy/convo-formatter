03188c0e7ed9d4dec1b77c3603ffaab420ce2587
jamoozy <jamoozy@ef3b9d53-392b-0410-a895-1f90f0f74573>
2009-06-04 00:41:09 +0000

    1) Started writing reader for Pidgin's text log format.   2) Noticed a lot of shared code & farmed it out to an abstract class.

---
+++ trunk/src/reader/PidginTextReader.java
@@ -0,0 +1,14 @@
+package reader;
+
+import java.io.IOException;
+import java.util.Vector;
+
+public class PidginTextReader extends AbstractReader
+{
+  public PidginTextReader() {}
+
+  protected void _parseLine(int lineNumber, String line) throws FileFormatException
+  {
+    System.err.println("Method not implemented: PidginTextReader._parseLine()!");
+  }
+}
